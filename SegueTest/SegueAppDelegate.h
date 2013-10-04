a1e4337c16d528ba060b460c606c02336a1943ca
tylacock <tylacock@gmail.com>
2013-10-04 15:29:57 -0400

    Initial Commit

---
+++ SegueTest/SegueAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SegueAppDelegate.h
+//  SegueTest
+//
+//  Created by Lacock, Ty on 10/4/13.
+//  Copyright (c) 2013 TWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SegueAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SegueTest/SegueViewController.m
@@ -0,0 +1,29 @@
+//
+//  SegueViewController.m
+//  SegueTest
+//
+//  Created by Lacock, Ty on 10/4/13.
+//  Copyright (c) 2013 TWL. All rights reserved.
+//
+
+#import "SegueViewController.h"
+
+@interface SegueViewController ()
+
+@end
+
+@implementation SegueViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
